7662edea12feae4cfaff99a727bc203610c62c24
git504 <git504@mailo.com>
2021-07-01 14:09:24 +0200

    js6/6

--- JS-A-to-Z-support/6 SUPPORT/cours/index.js
+++ JS-A-to-Z-support/6 SUPPORT/cours/index.js
@@ -1,30 +1,60 @@
+"use strict";
 // CANVAS
 //-------
 
-function draw() {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+// function draw() {
+//   const canvas = document.getElementById("canvas");
+//   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect(50, 50, 50, 150);
+//   ctx.fillStyle = "rgb(200,0,0)";
+//   ctx.fillRect(50, 50, 50, 150);
 
-  ctx.fillStyle = "rgba(0,100,200, 0.5)";
-  ctx.fillRect(100, 30, 100, 50);
+//   ctx.fillStyle = "rgba(0,100,200,0.5)";
+//   ctx.fillRect(100, 30, 100, 50);
 
-  ctx.fillStyle = "rgba(100,100,200, 0.5)";
-  ctx.fillRect(10, 25, 100, 100);
-  ctx.clearRect(35, 25, 40, 40);
-  ctx.strokeRect(125, 75, 50, 50);
+//   ctx.fillStyle = "rgba(100,100,200, 0.5)";
+//   ctx.fillRect(10, 25, 100, 100);
+//   ctx.clearRect(35, 25, 40, 40);
+//   ctx.strokeRect(125, 75, 50, 50);
 
-  ctx.fillStyle = "rgba(200,100,200, 0.8)";
-  ctx.beginPath();
-  ctx.moveTo(180, 150);
-  ctx.lineTo(100, 75);
-  ctx.lineTo(100, 150);
-  ctx.fill();
-}
-window.addEventListener("load", draw);
+//   ctx.fillStyle = "rgba(200,100,200, 0.8)";
+//   ctx.beginPath();
+//   ctx.moveTo(180, 150);
+//   ctx.lineTo(100, 75);
+//   ctx.lineTo(100, 150);
+//   ctx.fill();
+// }
+
+// window.addEventListener("load", draw);
 
 //------------
 // TRY / CATCH
-//----------
+//------------
+
+try {
+  // Test un block de code
+  maFonction();
+} catch (err) {
+  // console.log(err);
+}
+
+function isValidJSON(txt) {
+  try {
+    JSON.parse(txt);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+// console.log(isValidJSON());
+
+// Finally
+try {
+  // Test un block de code
+  // maFonction();
+} catch (err) {
+  console.log(err);
+} finally {
+  // console.log("on est arrivé au bout !");
+}
