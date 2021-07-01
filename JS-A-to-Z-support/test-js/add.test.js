9bf9e6c1aa60c454e67ca06c5070fa9cdd918d29
git504 <git504@mailo.com>
2021-07-01 16:06:24 +0200

    js6/6

--- JS-A-to-Z-support/test-js/add.test.js
+++ JS-A-to-Z-support/test-js/add.test.js
@@ -2,5 +2,6 @@ const addNum = require("./add");
 
 test("Additionne 2 nombres", () => {
   expect(addNum(1, 2)).toBe(3);
-//   expect(addNum(2, -4)).toBe(-2);
+  expect(addNum(2, -4)).toBe(-2);
 });
+
